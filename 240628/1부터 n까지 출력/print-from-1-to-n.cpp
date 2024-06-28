bedd700f160ee381f0793760b7eeeb47421c1e4f
Candy <a2208882@naver.com>
2024-06-28 22:17:54 +0900

    Title: 1부터 n까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240628/1부터 n까지 출력/print-from-1-to-n.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for(int i =1; i<a+1; i++ ){
+        cout << i << " ";
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
