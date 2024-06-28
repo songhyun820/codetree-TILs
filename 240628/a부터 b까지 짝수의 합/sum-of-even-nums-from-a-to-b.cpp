c8053f6abcee566e1d68491bc4458678841df744
Candy <a2208882@naver.com>
2024-06-28 22:47:41 +0900

    Title: a부터 b까지 짝수의 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240628/a부터 b까지 짝수의 합/sum-of-even-nums-from-a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() { 
+    int a, b, i, sum = 0;
+
+    cin >> a >> b;
+    for(i = a; i < b; i++){
+        // if(i % 2 == 0){
+        //     sum += i;
+        // } 
+        i % 2 == 0 ? sum+=i : sum = sum;
+    }
+    cout << sum;
+    return 0;
+}
